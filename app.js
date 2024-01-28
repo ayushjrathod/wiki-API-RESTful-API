d23e7569914ea51288463380aa1b9e319f351efc
Ayush Rathod <111508368+ayushjrathod@users.noreply.github.com>
2024-01-28 14:28:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,101 @@
+//Initalization
+
+const express=require("express");
+const bodyParser=require("body-parser");
+const ejs=require("ejs");
+const mongoose=require("mongoose");
+
+const app = express();
+
+app.set('view engine','ejs');
+
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static("public"));
+
+//Setting up database usign monogoDB
+
+mongoose.connect("mongodb+srv://clashofa1057:temppass@cluster0.fqzvj3u.mongodb.net/RESTfulAPI?retryWrites=true&w=majority")
+    .then(()=>{console.log("mongodb connect success")})
+    .catch((err)=>{console.log(err)});
+
+const articleSchema = new mongoose.Schema({
+    title:String,
+    content:String
+});
+
+const Article = mongoose.model("Article",articleSchema);
+
+
+//Handling Requests 
+
+/////////////Requests Trageting all articles ///////////////////////
+app.route("/articles")
+.get((req,res)=>{
+    Article.find({})
+        .then((foundArticles)=>{
+            res.send(foundArticles);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+})
+
+.post((req,res)=>{
+
+    const newArticle = new Article({
+        title: req.body.title,
+        content:req.body.content
+    });
+
+    newArticle.save()
+        .then(()=>{
+            res.send("save success");
+        })
+        .catch((err)=>{
+            res.send(err);
+        });
+})
+
+.delete((req,res)=>{
+    Article.deleteMany()
+        .then(()=>{
+            res.send("delete success");
+        })
+        .catch((err)=>{
+            res.send(err);
+        })
+});
+
+/////////////Requests Trageting specific articles ///////////////////////
+
+app.route("/articles/:articleTitle")
+
+    .get((req,res)=>{
+        Article.findOne({title: req.params.articleTitle})
+            .then((foundArticles)=>{
+                if(foundArticles){
+                    res.send(foundArticles);
+                }else{
+                    res.send("No matching articles found");
+                }
+            })
+            .catch((err)=>{
+                console.log(err);
+            });
+    })
+
+    .put((req,res)=>{
+        Article.update(
+            {title: req.params.articleTitle},
+            {title: req.body.title,content: req.body.content}
+        )
+    })
+
+
+app.listen("3000",()=>{
+    console.log("Server started on port 3000");
+});
